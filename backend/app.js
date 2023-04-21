5e328121def91413daa4a49c09e6e58d4cac0dae
Mehdi <68072289+mehdi2202@users.noreply.github.com>
2023-04-21 14:23:29 +0200

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const helmet = require("helmet");
+
+//importation des routes
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const commentRoutes = require("./routes/comment");
+const likeRoutes = require("./routes/like");
+
+app.use(express.json());
+app.use(helmet());
+
+//en têtes HTTP CORS (cross origin ressources sharing)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+  next();
+});
+
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+app.use("/api/like", likeRoutes);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
