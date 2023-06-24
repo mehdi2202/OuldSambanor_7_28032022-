37e179c26c03d2fa25aeb42f6c564acc72869256
Mehdi <68072289+mehdi2202@users.noreply.github.com>
2023-06-24 16:18:35 +0200

    Delete backend/routes directory

--- backend/routes/like.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const likeCtrl = require('../controllers/like');
-const auth = require('../middleware/auth');
-
-router.post('/:postId', auth, likeCtrl.likePost);
-
-module.exports = router;
