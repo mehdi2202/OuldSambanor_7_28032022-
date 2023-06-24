942266383f5b81ad18ac383288b5f8f203e0267c
Mehdi <68072289+mehdi2202@users.noreply.github.com>
2023-06-24 16:19:26 +0200

    Add files via upload

---
+++ backend/routes/comment.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const commentCtrl = require("../controllers/comment");
+const auth = require("../middleware/auth");
+
+router.get("/", commentCtrl.getAllComments);
+router.post("/", commentCtrl.createComment);
+router.delete("/:id", auth, commentCtrl.deleteComment);
+
+module.exports = router;
---
+++ backend/routes/post.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const postCtrl = require("../controllers/post");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+// const multer = require("multer");
+// const upload = multer({ dest: "images/" });
+
+router.get("/", postCtrl.getAllPosts);
+router.post("/", auth, multer, postCtrl.createPost);
+router.put("/:id", auth, multer, postCtrl.updatePost);
+router.delete("/:id", auth, postCtrl.deletePost);
+
+module.exports = router;
